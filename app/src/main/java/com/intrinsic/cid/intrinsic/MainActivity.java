2cd8aaecd6f1f38800967a93a978231e58b187e1
Cid Khode <chidanandkhode@yahoo.com>
2018-02-25 17:06:06 -0500

    Initial commit

---
+++ app/src/main/java/com/intrinsic/cid/intrinsic/MainActivity.java
@@ -0,0 +1,27 @@
+package com.intrinsic.cid.intrinsic;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void loginPage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void signUpPage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(intent);
+    }
+}
