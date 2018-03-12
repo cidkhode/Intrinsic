f103b648b63d0674db961ea082def9f600fc1708
Connor Watson <connorwatson0811@gmail.com>
2018-03-12 14:30:00 -0400

    3/12 - Test commit. Added a comment to ForgotPassword.java

--- app/src/main/java/com/intrinsic/cid/intrinsic/ForgotPassword.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/ForgotPassword.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ForgotPassword extends AppCompatActivity {
-
+//This activity is for users who forgot their password
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
