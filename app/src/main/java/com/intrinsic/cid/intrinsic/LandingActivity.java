acb593022fda17e003e8ea91fbb445cfa08bd699
Connor Watson <connorwatson0811@gmail.com>
2018-03-18 22:32:59 -0400

    3/18 - Linking Activities Together

--- app/src/main/java/com/intrinsic/cid/intrinsic/LandingActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/LandingActivity.java
@@ -2,6 +2,7 @@ package com.intrinsic.cid.intrinsic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LandingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        final TextView user_name_display = (TextView) findViewById(R.id.user_name_display);
+        final TextView phone_number_display = (TextView) findViewById(R.id.phone_number_display);
     }
 }
--- app/src/main/java/com/intrinsic/cid/intrinsic/SignUpActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.intrinsic.cid.intrinsic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        final EditText create_phone_numnber = (EditText) findViewById(R.id.create_phone_number);
+        final EditText create_password = (EditText) findViewById(R.id.create_password);
+        final EditText create_name = (EditText) findViewById(R.id.create_name);
+        final EditText create_question = (EditText) findViewById(R.id.create_question);
+        final EditText create_answer = (EditText) findViewById(R.id.create_answer);
+        final EditText create_birthday = (EditText) findViewById(R.id.create_birthday);
+        final EditText create_email = (EditText) findViewById(R.id.create_email);
+
+        final Button create_user = (Button) findViewById(R.id.create_user);
+
     }
 }
