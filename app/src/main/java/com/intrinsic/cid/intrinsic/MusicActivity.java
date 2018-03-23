0d2a1e83e6fff9e9eb079c945025bbe5f55c8b46
Kevin Le <kwevinle@gmail.com>
2018-03-23 18:36:33 -0400

    Added spotify stuff

--- app/src/main/java/com/intrinsic/cid/intrinsic/MusicActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/MusicActivity.java
@@ -1,13 +1,33 @@
 package com.intrinsic.cid.intrinsic;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MusicActivity extends AppCompatActivity {
+
+    Button playlist1;
+    //adapterobject for list view
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
+
+        playlist1 =(Button)findViewById(R.id.playlist1);
+
+
+        playlist1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/album/54OXaFd0PtGtAgauTmARkD"));
+                startActivity(spotifyIntent);
+            }
+        });
+
     }
+
 }
