a8fc58bda2908dea8d5a7657ceb10a8877908458
Connor Watson <connorwatson0811@gmail.com>
2018-03-26 17:17:55 -0400

    3/26 - Updated Music Playlists.

--- app/src/main/java/com/intrinsic/cid/intrinsic/MusicActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/MusicActivity.java
@@ -5,29 +5,37 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 public class MusicActivity extends AppCompatActivity {
-
-    Button playlist1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music);
-
-        playlist1 =(Button)findViewById(R.id.playlist1);
-
-
-        playlist1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //replace link with any open.spotify playlist
-                Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/album/54OXaFd0PtGtAgauTmARkD"));
-                startActivity(spotifyIntent);
-            }
-        });
-
     }
-
+    public void openPlaylist1(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DWSqmBTGDYngZ?si=D7BfmebvRN24JxaclSmteA"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist2(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/thewolfeman99/playlist/68mX4U9iqFeibp1t9X82gy?si=PrKa9hKFQ1Wp_cpOpSNk2Q"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist3(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/1225813554/playlist/0cUoffCqblGCNdQftuCpSj?si=IJofllCkQtqvW3dWebd-oQ"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist4(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX2MyUCsl25eb?si=h5YIo_OOTryizuJXBV586A"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist5(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/intrinsiccafe/playlist/3ktL1Q2N0rsntPJpfYGuSB?si=kcefN64CRr26gTXXa2xudQ"));
+        startActivity(spotifyIntent);
+    }
 }
