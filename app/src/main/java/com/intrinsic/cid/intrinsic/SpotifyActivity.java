70e200599aa1a048618c43daec59b9fdc3d3ef78
Cid Khode <chidanandkhode@yahoo.com>
2018-03-27 23:59:32 -0400

    3/27 - Added navigation drawer for spotify.

--- app/src/main/java/com/intrinsic/cid/intrinsic/SpotifyActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/SpotifyActivity.java
@@ -1,10 +1,8 @@
 package com.intrinsic.cid.intrinsic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -13,23 +11,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.View;
 
-public class LandingPage extends AppCompatActivity
+public class SpotifyActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_landing_page);
-
-        final TextView user_name_display = (TextView) findViewById(R.id.user_name_display);
-        Intent intent = getIntent();
-        final String name = intent.getStringExtra("name");
-        final String phoneNumber = intent.getStringExtra("phoneNumber");
-
-        user_name_display.setText("Welcome " + name + "!");
+        setContentView(R.layout.activity_spotify);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -41,53 +31,6 @@ public class LandingPage extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        /*
-        final Button order_otg = (Button) findViewById(R.id.order_button);
-        order_otg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, OrderActivity.class);
-                intent.putExtra("phoneNumber", phoneNumber);
-                intent.putExtra("name", name);
-                LandingPage.this.startActivity(intent);
-            }
-        });
-        final Button musicBut = (Button) findViewById(R.id.music_button);
-        order_otg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, MusicActivity.class);
-                LandingPage.this.startActivity(intent);
-            }
-        });
-        final Button contactBut = (Button) findViewById(R.id.contact_us_button);
-        order_otg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LandingPage.this, ContactUsActivity.class);
-                LandingPage.this.startActivity(intent);
-            }
-        });*/
-    }
-
-    public void musicPage(View view)
-    {
-        Intent intent = new Intent(LandingPage.this, SpotifyActivity.class);
-        startActivity(intent);
-    }
-
-    public void contactUsPage(View view)
-    {
-        Intent intent = new Intent(LandingPage.this, ContactUsActivity.class);
-        startActivity(intent);
-    }
-
-    public void orderOTGPage(View view)
-    { //Here I need to pass in the phone number...
-        Intent intent = new Intent(LandingPage.this, OrderActivity.class);
-        startActivity(intent);
     }
 
     @Override
@@ -98,6 +41,13 @@ public class LandingPage extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.spotify, menu);
+        return true;
     }
 
     @Override
@@ -126,23 +76,54 @@ public class LandingPage extends AppCompatActivity
         } else if (id == R.id.menu_option) {
 
         } else if (id == R.id.order_option) {
-            startActivity(new Intent(LandingPage.this, OrderActivity.class));
+            startActivity(new Intent(SpotifyActivity.this, OrderActivity.class));
         } else if (id == R.id.rewards_option) {
 
         } else if (id == R.id.music_option) {
-            startActivity(new Intent(LandingPage.this, SpotifyActivity.class));
+            //do nothing
         } else if (id == R.id.specials_option) {
 
         } else if (id == R.id.contact_us_option) {
-            startActivity(new Intent(LandingPage.this, ContactUsActivity.class));
+            startActivity(new Intent(SpotifyActivity.this, ContactUsActivity.class));
         } else if (id == R.id.my_account_option) {
 
         } else if (id == R.id.logout_option) {
-            startActivity(new Intent(LandingPage.this, MainActivity.class));
+            startActivity(new Intent(SpotifyActivity.this, MainActivity.class));
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public void openPlaylist1(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DWSqmBTGDYngZ?si=D7BfmebvRN24JxaclSmteA"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist2(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/thewolfeman99/playlist/68mX4U9iqFeibp1t9X82gy?si=PrKa9hKFQ1Wp_cpOpSNk2Q"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist3(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/1225813554/playlist/0cUoffCqblGCNdQftuCpSj?si=IJofllCkQtqvW3dWebd-oQ"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist4(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/spotify/playlist/37i9dQZF1DX2MyUCsl25eb?si=h5YIo_OOTryizuJXBV586A"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist5(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/intrinsiccafe/playlist/3ktL1Q2N0rsntPJpfYGuSB?si=kcefN64CRr26gTXXa2xudQ"));
+        startActivity(spotifyIntent);
+    }
+    public void openPlaylist6(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://open.spotify.com/user/intrinsiccafe/playlist/3ktL1Q2N0rsntPJpfYGuSB?si=URMUNyBmT2a_ccg5qqIbdQ"));
+        startActivity(spotifyIntent);
     }
 }
