5d88bb21b039684ef6ba8a9ee7df5eb9b0cec52b
Kevin Le <kwevinle@gmail.com>
2018-03-30 14:39:28 -0400

    Updated Contact Us

--- app/src/main/java/com/intrinsic/cid/intrinsic/ContactActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/ContactActivity.java
@@ -1,6 +1,7 @@
 package com.intrinsic.cid.intrinsic;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class ContactActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -86,4 +88,24 @@ public class ContactActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void intrinsicCall(View view)
+    {
+        Intent callIntent = new Intent(Intent.ACTION_CALL);
+        callIntent.setData(Uri.parse("tel:2016182689"));
+        startActivity(callIntent);
+    }
+    public void intrinsicEmail(View view)
+    {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Uri data = Uri.parse("mailto:intrinsiccafe@gmail.com?subject=" + "" + "&body=" + "");
+        intent.setData(data);
+        startActivity(intent);
+    }
+    public void intrinsicWebsite(View view)
+    {
+        Intent spotifyIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.intrinsiccafe.com/"));
+        startActivity(spotifyIntent);
+    }
+
 }
