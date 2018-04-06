74503c77398cba90036f3d41d7bcd5a9d5ebbd65
Connor Watson <connorwatson0811@gmail.com>
2018-04-06 19:06:20 -0400

    4/6 - Edited Landing Page and Login...not functional yet

--- app/src/main/java/com/intrinsic/cid/intrinsic/LandingRequest.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/LandingRequest.java
@@ -11,11 +11,10 @@ public class LandingRequest extends StringRequest {
     private static final String SIGNUP_REQ_URL = "https://web.njit.edu/~cww5/intrinsic/edit_profile.php";
     private Map<String, String> params;
 
-    public LandingRequest(String oldNum, String phoneNumber, String password, String name, String secQues, String secAns,
+    public LandingRequest(String phoneNumber, String password, String name, String secQues, String secAns,
                          String birthdate, String email, Response.Listener<String> listener) {
         super(Method.POST, SIGNUP_REQ_URL, listener, null);
         params = new HashMap<>();
-        params.put("oldNum", oldNum);
         params.put("phoneNumber", phoneNumber);
         params.put("password", password);
         params.put("name", name);
