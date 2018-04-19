b4293bf890c8f7613932faf9ea6916c508461675
Cid Khode <chidanandkhode@yahoo.com>
2018-04-19 17:00:22 -0400

    - Sandbox PayPal ordering on the go (not finalized just yet)  - Removed minor bug in app_bar files

---
+++ app/src/main/java/com/intrinsic/cid/intrinsic/Payment/Payment.java
@@ -0,0 +1,6 @@
+package com.intrinsic.cid.intrinsic.Payment;
+
+public class Payment {
+    public static final String PAYPAL_CLIENT_ID = "Afy1S3I_-QqNzP0hGSw442Es41qkRxKbSPOKb4NZ_ah7G7QklwxOGFSxyL_ZkQmQk2Y_60sLj1CSfaFE";
+
+}
