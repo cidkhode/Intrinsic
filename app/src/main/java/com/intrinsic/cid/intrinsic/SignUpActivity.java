f260b866649d219e2d17538caf21f7e4275cdda3
Connor Watson <connorwatson0811@gmail.com>
2018-04-19 23:34:52 -0400

    4/19 - Updated Sign Up

--- app/src/main/java/com/intrinsic/cid/intrinsic/SignUpActivity.java
+++ app/src/main/java/com/intrinsic/cid/intrinsic/SignUpActivity.java
@@ -25,6 +25,7 @@ public class SignUpActivity extends AppCompatActivity {
 
         final EditText create_phone_number = (EditText) findViewById(R.id.create_phone_number);
         final EditText create_password = (EditText) findViewById(R.id.create_password);
+        final EditText confirm_password = (EditText) findViewById(R.id.confirm_password);
         final EditText create_name = (EditText) findViewById(R.id.create_name);
         final EditText create_question = (EditText) findViewById(R.id.create_question);
         final EditText create_answer = (EditText) findViewById(R.id.create_answer);
@@ -38,6 +39,7 @@ public class SignUpActivity extends AppCompatActivity {
             public void onClick(View view) {
                 final String phoneNumber = create_phone_number.getText().toString();
                 final String password = create_password.getText().toString();
+                final String confirmPassword = confirm_password.getText().toString();
                 final String name = create_name.getText().toString();
                 final String secQues = create_question.getText().toString();
                 final String secAns = create_answer.getText().toString();
@@ -47,23 +49,28 @@ public class SignUpActivity extends AppCompatActivity {
                 Response.Listener<String> responseListener = new Response.Listener<String>(){
                     @Override
                     public void onResponse(String response){
-                        try {
-                            JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success){
-                                Toast.makeText(SignUpActivity.this,"Signup Successful! If you don't see an email right away, please check your spam folder.", Toast.LENGTH_LONG).show();
-                                Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
-                                SignUpActivity.this.startActivity(intent);
-                            }
-                            else{
-                                String warnings = jsonResponse.getString("warnings");
-                                AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
-                                builder.setMessage(warnings).setNegativeButton("Retry", null).create().show();
-                                //Toast.makeText(SignUpActivity.this,"Signup Unsuccessful. Please Try Again!", Toast.LENGTH_LONG).show();
+                        if (password.equals(confirmPassword)) {
+                            try {
+                                JSONObject jsonResponse = new JSONObject(response);
+                                boolean success = jsonResponse.getBoolean("success");
+                                if (success) {
+                                    Toast.makeText(SignUpActivity.this, "Signup Successful! If you don't see an email right away, please check your spam folder.", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                                    SignUpActivity.this.startActivity(intent);
+                                } else {
+                                    String warnings = jsonResponse.getString("warnings");
+                                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                                    builder.setMessage(warnings).setNegativeButton("Retry", null).create().show();
+                                    //Toast.makeText(SignUpActivity.this,"Signup Unsuccessful. Please Try Again!", Toast.LENGTH_LONG).show();
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
                             }
                         }
-                        catch (JSONException e){
-                            e.printStackTrace();
+                        else
+                        {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                            builder.setMessage("Passwords must match!").setNegativeButton("Retry", null).create().show();
                         }
                     }
                 };
